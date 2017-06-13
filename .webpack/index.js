7b23786206268ec46674373b5325a5734ff18e6e
Ivan Mironchik <me.roaiven@gmail.com>
2017-06-13 16:42:56 +0300

    Integrate babel, webpack

---
+++ .webpack/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './static'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      }
+    ],
+  },
+};
