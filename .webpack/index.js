34a92d0d44d9ba157bb9d2b7d4613f9b6c921f1f
Ivan Mironchik <me.roaiven@gmail.com>
2017-06-17 21:42:47 +0300

    Integrate React

--- .webpack/index.js
+++ .webpack/index.js
@@ -30,7 +30,9 @@ export default {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: resolve("./src/index.html")
+    }),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV)
--- src/index.js
+++ src/index.js
@@ -8,4 +8,9 @@
  * @flow
  */
 
-console.log('Hello world!');
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <div>Hello world</div>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
