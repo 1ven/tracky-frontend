b3bd67af350ea3bbcd6695acd211c5c156f1f5b2
1ven <1ven.mironchik@gmail.com>
2017-07-31 19:47:36 +0300

    Upd createSafeSelector

--- src/core/utils/createSafeSelector.ts
+++ src/core/utils/createSafeSelector.ts
@@ -1,7 +1,10 @@
 import { Selector } from "swifty";
 import { Maybe } from "ramda-fantasy";
 
-export default <S, P>(selector: Selector<S, P>) => (state: S, props?: P) => {
+export default <S, P, R>(selector: Selector<S, P, R>) => (
+  state: S,
+  props?: P
+) => {
   const result = selector(state, props);
 
   if (typeof result === "undefined") {
