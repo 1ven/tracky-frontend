c3d5006ccbd8f3336f44490769017e62fe8c39b3
1ven <1ven.mironchik@gmail.com>
2017-08-05 14:50:50 +0300

    Add comment

--- src/modules/user/selectors.ts
+++ src/modules/user/selectors.ts
@@ -2,4 +2,7 @@ import { map, prop, compose, filter } from "ramda";
 // import { getDataSafe } from "core/selectors";
 
 const getNames = map(prop("name"));
+// TODO: keep generic selectors in api/repos
+// keep UI specific selectors in corresponding module, ex "user"
+
 // export const getRepos = compose(map(getNames), getDataSafe);
