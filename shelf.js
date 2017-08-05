9294a13ab7eaa82883fbc7947811177519902106
1ven <1ven.mironchik@gmail.com>
2017-08-05 12:40:40 +0300

    Update shelf interface

--- shelf.js
+++ shelf.js
@@ -1,14 +1,17 @@
-const middleware = createShelfMiddleware(rootApi, config, enhancer);
+const middleware = createShelfMiddleware(rootApi);
 
-const rootApi = {
+// redux-shelf should implement vanilla redux logic
+// if need normalizr support - do this with enhancers
+const rootApi = createApi({
   entities: {
     tickets: {
       readAll: {
         url: "/entities/tickets/",
         method: "GET",
-        reduce: {
-          [type(readAllTickets, "request")]: (state, action) => state
-        },
+        // prob have regular reducer
+        // reduce: {
+        //   [type(readAllTickets, "request")]: (state, action) => state
+        // },
         config
       }
     },
@@ -19,17 +22,62 @@ const rootApi = {
       }
     }
   }
-};
+}, config, enhancer);
+
+const reducer = (state, action) => {}
+const reducer2 = (state, action) => {}
+
+const mergedReducer = mergeReducers(reducer, reducer2);
+
+// check redux middlewares and enhancers
+
+const enhancer1 = (spec) => (api) => {
+  const mergedReducer = mergeReducers(api.reducer, spec.reducer);
+
+  return {
+    ...api,
+    reducer: mergedReducer
+  }
+}
+
+// should have hooks?
+const enhancer2 = (spec) => (api) => {
+  const schema = spec.schema;
+
+  return {
+
+  }
+}
+
+const enhancer3 = (spec) => (api) => {
+  const headers = spec.headers;
+
+  return {
+    ...api,
+    call: () => axios()
+  }
+}
+
+// rootApi = {
+//   readAll: {
+//     path: 'api/entities/tickets/readAll'
+//     selectors: {
+//       isFetching: ...
+//     },
+//     reducer,
+//     call
+//   }
+// }
 
 const readAllTickets = api => api.entities.tickets.readAll;
 
 dispatch(
-  request(readAllTickets, {
+  request(readAllTickets(rootApi), {
     params: {},
     body: {}
   })
 );
 
-select(readAllTickets, "isFetching");
+select(readAllTickets(rootApi), "isFetching");
 
-type(readAllTickets, "request");
+type(readAllTickets(rootApi), "request");
