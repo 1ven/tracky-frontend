3ccbccc35a25f23a4884816109689b2ff75c111b
1ven <1ven.mironchik@gmail.com>
2017-08-27 14:25:29 +0300

    Fix active state updating

--- src/components/global/Overlay/Sidebar/Projects/Provider.ts
+++ src/components/global/Overlay/Sidebar/Projects/Provider.ts
@@ -2,6 +2,12 @@ import { connect } from "core/redux";
 import { modals, actions } from "components/global/Modals";
 import View from "./View";
 
-export default connect(void 0, {
-  onAdd: () => actions.showModal(modals.CREATE_PROJECT)
-})(View);
+export default connect(
+  void 0,
+  {
+    onAdd: () => actions.showModal(modals.CREATE_PROJECT)
+  },
+  {
+    pure: false
+  }
+)(View);
