6af754635bf12d105ab5e65514d15d568fe2de6b
1ven <1ven.mironchik@gmail.com>
2017-09-08 14:15:57 +0300

    Use why did you update package

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,17 @@ import { Provider } from "./core/redux";
 import Global from "./components/global";
 import Pages from "./components/pages";
 
+if (process.env.NODE_ENV !== "production") {
+  let createClass = React.createClass;
+  Object.defineProperty(React, "createClass", {
+    set: nextCreateClass => {
+      createClass = nextCreateClass;
+    }
+  });
+  const { whyDidYouUpdate } = require("why-did-you-update");
+  whyDidYouUpdate(React);
+}
+
 export const App = nest(Provider, Global, Pages);
 
 ReactDOM.render(<App />, document.getElementById("root"));
