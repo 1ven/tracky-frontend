a15a058a88dbd21e430dd5698d052e8d1564fcb8
1ven <1ven.mironchik@gmail.com>
2017-09-08 14:25:09 +0300

    Move withClickOutside to another Provider

--- src/components/shared/kit/InlineEdit/Form/Provider.ts
+++ src/components/shared/kit/InlineEdit/Form/Provider.ts
@@ -1,7 +1,12 @@
+import { compose } from "recompose";
 import { withForm } from "core/form";
+import { withClickOutside } from "core/decorators";
 import View, { Props as ViewProps } from "./View";
 
-export default withForm()(View);
+export default compose<ViewProps, Props>(
+  withClickOutside(p => p.hide()),
+  withForm()
+)(View);
 
 export type Props = ViewProps & {
   form: string;
--- src/components/shared/kit/InlineEdit/Provider.ts
+++ src/components/shared/kit/InlineEdit/Provider.ts
@@ -1,7 +1,6 @@
 import { prop, concat } from "ramda";
 import { compose, withProps, withState, withHandlers } from "recompose";
 import { connect } from "core/redux";
-import { withClickOutside } from "core/decorators";
 import { show, hide } from "./actions";
 import { isEditing } from "./reducer";
 import View from "./View";
@@ -19,6 +18,5 @@ export default compose(
         dispatch(hide(name));
       }
     })
-  ),
-  withClickOutside(p => p.hide())
+  )
 )(View) as any;
